673ed790781b52d18f4202041ba12283901b6b9c
wataru nakazato <n16007@std.it-college.ac.jp>
2016-07-27 11:15:03 +0900

    追記した

---
+++ chapter9/animals.py
@@ -0,0 +1,21 @@
+from . level2 import Animate
+
+
+class Animals(Animate):
+    pass
+
+
+class Mammals(Animals):
+    pass
+
+
+class Giraffes(Mammals):
+    pass
+
+
+class Cat(Mammals):
+    pass
+
+
+class Human(Mammals):
+    pass
